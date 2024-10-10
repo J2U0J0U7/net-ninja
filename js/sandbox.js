e5863ee6d4c048649933f1eb7c494062e87b4a04
Julian <126jisaacs@frhsd.com>
2024-10-10 12:07:43 -0400

    10/10/2024

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,13 +1,22 @@
-//Strings
+let ninjas = ['shaun', 'ryu', 'chun-li'];
 
+//ninjas[1] = 'ken';
+// console.log(ninjas[1]);
 
-//string concatenation
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2]);
 
+// let random = ['shaun'], 'crystal', 30, 20];
+// console.log(random);
 
-//getting characters
+//console.log(ninjas.length);
 
+// array methods
 
-//string length
+//let result = ninjas.join('-');
+//let result = ninjas.indexOf(' chun-li ');
+//let result = ninjas.concat(['ken', 'crystal']);
+let result = ninjas.push('ken');
+result = ninjas.pop();
 
-
-//string methods
+console.log(result);
